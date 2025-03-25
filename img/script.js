d9a82dcce95dfcdb5e10a4b2e72a4c0135097395
robinho-c <159184830+robinho-c@users.noreply.github.com>
2025-03-25 10:54:19 -0300

    teste

---
+++ img/script.js
@@ -0,0 +1,8 @@
+document .addEventListener('DOMcontentLoaded, function'()) {
+    const AumentaFonteBotao = document. getElementById("aumentar-font")
+    const DiminuiFonteBotao = document. getElementById('diminui-font')
+
+}
+let tamanhoatualdafonte=1
+AumentaFonteBotao + = 0,1;
+document.body.style.fontSize='$(tamanhoAtualFonte)rem'
